8d4fe158b09e731e4fd2b79a554760410e1ed498
Diego Said Anaya Mancilla <redcpp@gmail.com>
2020-06-18 00:30:00 -0500

    Initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Algorand />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Algorand from './components/Algorand.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Algorand
   }
 }
 </script>
---
+++ src/components/Algorand.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Welcome</h1>
+    <p>Status: {{ status }}</p>
+  </div>
+</template>
+
+<script>
+const algosdk = require('algosdk');
+const baseServer = 'https://testnet-algorand.api.purestake.io/ps1';
+const port = '';
+const token = {
+    'X-API-Key': 'YOUR API KEY'
+}
+
+// eslint-disable-next-line no-unused-vars
+
+export default {
+  async asyncData () {
+    const algodClient = new algosdk.Algod(token, baseServer, port);
+    let status = await algodClient.status();
+    console.log("Algorand network status: %o", status);
+    return { status }
+  }
+}
+</script>
