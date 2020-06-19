7bf8d028f3cb81b505915e690f2c3b7a78603c95
Diego Said Anaya Mancilla <redcpp@gmail.com>
2020-06-18 23:24:11 -0500

    Finish my first algorand tutorial

--- src/components/Algorand.vue
+++ src/components/Algorand.vue
@@ -1,26 +1,85 @@
 <template>
-  <div>
-    <h1>Welcome</h1>
-    <p>Status: {{ status }}</p>
+  <div class="container">
+    <h1>Last block visualizer</h1>
+    <VueP5 @setup="setup" @draw="draw"></VueP5>
   </div>
 </template>
 
 <script>
-const algosdk = require('algosdk');
-const baseServer = 'https://testnet-algorand.api.purestake.io/ps1';
-const port = '';
-const token = {
-    'X-API-Key': 'YOUR API KEY'
-}
+import VueP5 from "vue-p5";
 
-// eslint-disable-next-line no-unused-vars
+const pureStakeKey = "A4cepxeclB6jMPj2L6CXt2aZapaJqgyQ7wgMp9xA";
+const algosdk = require("algosdk");
+// const main = require('algosdk/src/main');
+const baseServer = "https://testnet-algorand.api.purestake.io/ps1";
+const port = "";
+const token = {
+  "X-API-Key": pureStakeKey
+};
 
 export default {
-  async asyncData () {
-    const algodClient = new algosdk.Algod(token, baseServer, port);
-    let status = await algodClient.status();
-    console.log("Algorand network status: %o", status);
-    return { status }
+  name: "AlgorandP5",
+  components: {
+    VueP5
+  },
+  data: () => ({
+    status: {},
+    w: 600,
+    h: 600,
+    size: 20,
+    blocks: []
+  }),
+  async mounted() {
+    this.updateStatus();
+    setInterval(() => {
+      this.updateStatus()
+    }, 1000);
+  },
+  methods: {
+    setup(sketch) {
+      sketch.createCanvas(this.w, this.h);
+      sketch.frameRate(1);
+      sketch.colorMode(sketch.HSB, 100);
+      sketch.background(255);
+      sketch.noStroke();
+    },
+    draw(sketch) {
+      sketch.background(0,0,100,50);
+      this.blocks.forEach(block => {
+        block.y += 1;
+        sketch.fill(block.color.r, block.color.g, block.color.b);
+        sketch.rect(
+          block.x * this.size,
+          block.y * this.size,
+          this.size,
+          this.size
+        );
+      });
+    },
+    getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    },
+    async updateStatus() {
+      const algodClient = new algosdk.Algod(token, baseServer, port);
+      let status = await algodClient.status();
+      if (
+        !this.blocks.length ||
+        this.blocks[this.blocks.length - 1].id !== status.lastRound
+      ) {
+        const newBlock = {
+          id: status.lastRound,
+          y: 0,
+          x: this.getRandomInt(this.w / this.size),
+          color: {
+            r: this.getRandomInt(100),
+            g: this.getRandomInt(100),
+            b: this.getRandomInt(100)
+          }
+        };
+        console.log(newBlock);
+        this.blocks.push(newBlock);
+      }
+    }
   }
-}
+};
 </script>
